9ebea7115a01f55db95a6fc3e5d92ef0f5e396b3
Tullio <tullio.canepa@gmail.com>
2016-06-01 23:30:03 +0200

    Initiated support for snapshot load/store

--- src/org/helios/core/snapshot/SnapshotFile.java
+++ src/org/helios/core/snapshot/SnapshotFile.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 public class SnapshotFile implements Snapshot
 {
     @Override
+    public void load() throws IOException
+    {
+
+    }
+
+    @Override
     public void save() throws IOException
     {
 
